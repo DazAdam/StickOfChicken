14eb3b7477aff6becb11a14d0ee53fc6d6485816
DazAdam <dazadam@outlook.sg>
2016-05-29 20:00:02 +0800

    Create poop.js

---
+++ poop.js
@@ -0,0 +1,12 @@
+function procCmd(cmd) {
+    cmd = cmd.toLowerCase();
+    if (cmd == "iamcool"){
+        clientMessage("How could you?");
+        getPlayer->setHealth(0);
+        
+    }
+    if (cmd == "adamiscool"){
+        clientMessage("Thank you! :D");
+        getPlayer->addItemInventory(264, 64)
+}
+}
